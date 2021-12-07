7193c4f274370f954c5a2c43fc087f2219923707
Fernando Henrique <cara-se@hotmail.com>
2021-12-07 02:06:07 -0300

    Padrão Builder em Java

---
+++ fogueteBuilder.java
@@ -0,0 +1,26 @@
+package com.company.build;
+
+public class fogueteBuilder {
+    private String modelo;
+    private String motores;
+    private int assentos;
+
+    public fogueteBuilder Modelo(String modelo) {
+        this.modelo = modelo;
+        return this;
+    }
+
+    public fogueteBuilder Motores(String motores) {
+        this.motores = motores;
+        return this;
+    }
+
+    public fogueteBuilder Assentos(int assentos) {
+        this.assentos = assentos;
+        return this;
+    }
+
+    public foguete createFoguete() {
+        return new foguete(modelo, motores, assentos);
+    }
+}
